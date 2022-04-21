d60d6157700f6299560befc0c34db035608a968d
Dylan Nguyen <dylannguyen@macbookpro.socal.rr.com>
2022-04-21 08:16:04 -0700

    created initial hastag searcher, imported tweepy, need to access API

---
+++ twitter-hastag-scraper.py
@@ -0,0 +1,10 @@
+import tweepy
+
+c_key = "Consumer Key"
+c_secret = "Consumer Secret Key"
+a_key = "Access Key"
+a_secret = "Access Secret Key"
+
+
+#to access Twitter API, we need to go to your personal Twitter App page and find the keys in "Keys and Access Tokens" tab
+def get_tweets(text):
